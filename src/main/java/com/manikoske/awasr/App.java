d88e3fac55beaebc9af0031218b082120be4d976
marek kovacik <marek.kovacik@atos.net>
2016-08-22 13:38:28 +0200

    JAK added

--- src/main/java/com/manikoske/awasr/App.java
+++ src/main/java/com/manikoske/awasr/App.java
@@ -1,13 +1,19 @@
 package com.manikoske.awasr;
 
+import de.micromata.opengis.kml.v_2_2_0.Kml;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws FileNotFoundException {
+        final Kml kml = new Kml();
+        kml.createAndSetPlacemark().withName("London, UK").withOpen(Boolean.TRUE).createAndSetPoint().addToCoordinates(-0.126236, 51.500152);
+        kml.marshal(new File("HelloKml.kml"));
     }
 }
