79007ed42e7b89cd048e418ccf2311037d65887f
marek kovacik <marek.kovacik@atos.net>
2016-08-23 10:58:12 +0200

    ...

--- src/main/java/com/manikoske/awasr/App.java
+++ src/main/java/com/manikoske/awasr/App.java
@@ -1,6 +1,6 @@
 package com.manikoske.awasr;
 
-import de.micromata.opengis.kml.v_2_2_0.Kml;
+import de.micromata.opengis.kml.v_2_2_0.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -12,8 +12,33 @@ import java.io.FileNotFoundException;
 public class App 
 {
     public static void main( String[] args ) throws FileNotFoundException {
-        final Kml kml = new Kml();
-        kml.createAndSetPlacemark().withName("London, UK").withOpen(Boolean.TRUE).createAndSetPoint().addToCoordinates(-0.126236, 51.500152);
+        /* https://github.com/micromata/javaapiforkml/blob/master/src/test/java/de/micromata/jak/examples/Example3.java */
+        /* https://github.com/micromata/javaapiforkml/blob/master/src/test/java/de/micromata/jak/examples/Example1.java */
+
+        Kml kml = Kml.unmarshal(new File("src/main/java/com/manikoske/awasr/kmz/input/alHailAlJanubiyya.kml"));
+
+        Document document = (Document) kml.getFeature();
+        Folder folder = (Folder) document.getFeature().get(0);
+
+        final Kml outputKml = new Kml();
+        Document outputDocument = outputKml.createAndSetDocument().withName("JAK Example1").withOpen(true);
+        Style outputStyle = outputDocument.createAndAddStyle();
+
+        // create a Folder
+        Folder outputFolder = outputDocument.createAndAddFolder();
+        folder.withName("Continents with Earth's surface").withOpen(true);
+
+
+//        kml = Kml.unmarshal(new File("src/main/java/com/manikoske/awasr/kmz/input/alHailAlShamaliyya.kml"));
+//        kml = Kml.unmarshal(new File("src/main/java/com/manikoske/awasr/kmz/input/alKhaud.kml"));
+//        kml = Kml.unmarshal(new File("src/main/java/com/manikoske/awasr/kmz/input/alMabailaAlJanubiyya.kml"));
+//        kml = Kml.unmarshal(new File("src/main/java/com/manikoske/awasr/kmz/input/alMabailaAlShamaliyya.kml"));
+//        kml = Kml.unmarshal(new File("src/main/java/com/manikoske/awasr/kmz/input/alMawalihAlJanubiyya.kml"));
+//        kml = Kml.unmarshal(new File("src/main/java/com/manikoske/awasr/kmz/input/alMawalihAlShamaliyya.kml"));
+//        kml = Kml.unmarshal(new File("src/main/java/com/manikoske/awasr/kmz/input/alSeebJadida.kml"));
+//        kml = Kml.unmarshal(new File("src/main/java/com/manikoske/awasr/kmz/input/alSharadi.kml"));
+//        kml = Kml.unmarshal(new File("src/main/java/com/manikoske/awasr/kmz/input/hayAlSaruj.kml"));
+//        kml = Kml.unmarshal(new File("src/main/java/com/manikoske/awasr/kmz/input/surElHadid.kml"));
         kml.marshal(new File("HelloKml.kml"));
     }
 }
