25cc3f9d535c2934f84c28cc4064bedd4e25321f
Marek Kovacik <manikoske@gmail.com>
2016-08-27 12:16:54 +0200

    poriadky

--- src/main/java/com/manikoske/awasr/App.java
+++ src/main/java/com/manikoske/awasr/App.java
@@ -1,9 +1,20 @@
 package com.manikoske.awasr;
 
+import com.sun.xml.bind.marshaller.NamespacePrefixMapper;
 import de.micromata.opengis.kml.v_2_2_0.*;
 
+import javax.print.Doc;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Hello world!
@@ -11,22 +22,101 @@ import java.io.FileNotFoundException;
  */
 public class App 
 {
-    public static void main( String[] args ) throws FileNotFoundException {
-        /* https://github.com/micromata/javaapiforkml/blob/master/src/test/java/de/micromata/jak/examples/Example3.java */
-        /* https://github.com/micromata/javaapiforkml/blob/master/src/test/java/de/micromata/jak/examples/Example1.java */
 
-        Kml kml = Kml.unmarshal(new File("src/main/java/com/manikoske/awasr/kmz/input/alHailAlJanubiyya.kml"));
+    public static final String IMAGE_PATH = "placemark.png";
+    public static final String ICON_STYLE_NAME = "style_icon";
+    public static final String BASE_INPUT_PATH = "kmz/input/";
 
-        Document document = (Document) kml.getFeature();
-        Folder folder = (Folder) document.getFeature().get(0);
+    public static final String DATA_NAME_FIBRE_HUB = "fibreHub";
+    public static final String DATA_NAME_NETWORK_ACCESS_POINT = "networkAccessPoint";
+    public static final String DATA_NAME_SERIAL = "serial";
+    public static final String DATA_NAME_TAG_ID = "tagId";
 
-        final Kml outputKml = new Kml();
-        Document outputDocument = outputKml.createAndSetDocument().withName("JAK Example1").withOpen(true);
-        Style outputStyle = outputDocument.createAndAddStyle();
 
-        // create a Folder
-        Folder outputFolder = outputDocument.createAndAddFolder();
-        folder.withName("Continents with Earth's surface").withOpen(true);
+
+    public static void main( String[] args ) throws FileNotFoundException, JAXBException {
+
+        Marshaller m =  JAXBContext.newInstance(new Class[] { Kml.class }).createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true );
+        m.setProperty(Marshaller.JAXB_FRAGMENT, false );
+        m.setProperty(javax.xml.bind.Marshaller.JAXB_ENCODING, "UTF-8");
+        NamespacePrefixMapper mapper = new NamespacePrefixMapper() {
+            @Override
+            public String getPreferredPrefix(String s, String s1, boolean b) {
+                if (s.matches("http://www.opengis.net/kml/2.2")) {
+                    return "";
+                }
+                return null;
+            }
+        };
+        m.setProperty("com.sun.xml.bind.namespacePrefixMapper", mapper);
+
+        Map<String, List<String>> resultMapping = new HashMap<>();
+//        resultMapping.put("Al Seeb", Arrays.asList("alSharadi.kml", "alSeebJadida.kml", "surElHadid.kml"));
+        resultMapping.put("Al Khod", Arrays.asList("alKhaud.kml"));
+
+        String resultName;
+        List<String> inputFiles;
+        Kml inputKml;
+        Document inputDocument;
+        Folder inputFolder;
+        Placemark inputPlacemark;
+        Point inputPoint;
+        Coordinate inputCoordinate;
+
+        Kml outputKml;
+        Document outputDocument;
+        Placemark outputPlacemark;
+
+        StringBuilder sb;
+
+        for (Map.Entry<String, List<String>> entry : resultMapping.entrySet()) {
+            resultName = entry.getKey();
+            inputFiles = entry.getValue();
+
+            outputKml = new Kml();
+            outputDocument = outputKml.createAndSetDocument().withName(resultName);
+            Style outputStyle = outputDocument.createAndAddStyle();
+            Icon icon = new Icon().withHref(IMAGE_PATH);
+            outputStyle.withId(ICON_STYLE_NAME).createAndSetIconStyle().withScale(2).withIcon(icon);
+
+            for (String inputFileName : inputFiles) {
+                inputKml = Kml.unmarshal(new File(BASE_INPUT_PATH + inputFileName));
+                inputDocument = (Document) inputKml.getFeature();
+                for (Feature folderFeature : inputDocument.getFeature()) {
+                    inputFolder = (Folder) folderFeature;
+                    for (Feature placemarkFeature : inputFolder.getFeature()) {
+                        inputPlacemark = (Placemark) placemarkFeature;
+                        inputPoint = (Point) inputPlacemark.getGeometry();
+                        inputCoordinate = inputPoint.getCoordinates().iterator().next();
+
+                        sb = new StringBuilder();
+
+                        for (SimpleData simpleData : inputPlacemark.getExtendedData().getSchemaData().iterator().next().getSimpleData()) {
+                            switch (simpleData.getName()) {
+                                case "FDH":
+                                case "NAP":
+                                case "TENANCIES":
+                                    sb.append(simpleData.getValue());
+                                    sb.append("~");
+                                    break;
+                            }
+                        }
+                        sb.append(inputPlacemark.getName());
+                        sb.append("~");
+                        sb.append(inputCoordinate.getLongitude());
+                        sb.append("~");
+                        sb.append(inputCoordinate.getLatitude());
+
+                        outputPlacemark = outputDocument.createAndAddPlacemark();
+                        outputPlacemark.withName(inputPlacemark.getName()).withStyleUrl("#" + ICON_STYLE_NAME);
+                        outputPlacemark.createAndSetPoint().addToCoordinates(inputCoordinate.getLongitude(), inputCoordinate.getLatitude(), 0);
+                        outputPlacemark.setDescription(sb.toString());
+                    }
+                }
+            }
+            m.marshal(outputKml, new FileOutputStream(new File(resultName + ".kml")));
+        }
 
 
 //        kml = Kml.unmarshal(new File("src/main/java/com/manikoske/awasr/kmz/input/alHailAlShamaliyya.kml"));
@@ -39,6 +129,6 @@ public class App
 //        kml = Kml.unmarshal(new File("src/main/java/com/manikoske/awasr/kmz/input/alSharadi.kml"));
 //        kml = Kml.unmarshal(new File("src/main/java/com/manikoske/awasr/kmz/input/hayAlSaruj.kml"));
 //        kml = Kml.unmarshal(new File("src/main/java/com/manikoske/awasr/kmz/input/surElHadid.kml"));
-        kml.marshal(new File("HelloKml.kml"));
+
     }
 }
--- src/main/java/com/manikoske/awasr/html/test.js
+++ src/main/java/com/manikoske/awasr/html/test.js
@@ -4,13 +4,19 @@ if (typeof(networkAreas) == 'undefined') {
 
     networkAreas.SERVICE_HTML_URL = "register.html?";
     networkAreas.REGISTRATION_FRAGMENT = "#registration-block";
-    networkAreas.KMZ_BASE_URL = "http://www.awasr.om/KMZ/";
+    // networkAreas.KMZ_BASE_URL = "http://www.awasr.om/KMZ/";
+    // networkAreas.KMZ_BASE_URL = "https://www.dropbox.com/s/ztr5j6tplyryaxu/";
+    networkAreas.KMZ_BASE_URL = "https://www.dropbox.com/s/df5e3zggl4la9zh/";
     networkAreas.MAP_ELEMENT_ID = "map_canvas";
+
+    // https://www.dropbox.com/s/df5e3zggl4la9zh/Al-Koudh2.zip?dl=0
+
 
     networkAreas.areas = {
         alKoudh : {
             title: "Al-Koudh",
-            kmlUrlSuffix: "Al-Koudh.zip",
+            // kmlUrlSuffix: "Al-Koudh.zip",
+            kmlUrlSuffix: "Al-Koudh2.zip?dl=1",
             lat: 23.627010,
             lng: 58.206414,
         },
@@ -22,15 +28,51 @@ if (typeof(networkAreas) == 'undefined') {
         return networkAreas.KMZ_BASE_URL + networkAreas.currentlySelected.kmlUrlSuffix;
     }
 
+
+    networkAreas.initialize = function() {
+        var myOptions = {
+            zoom: 14,
+            center: new google.maps.LatLng(networkAreas.currentlySelected.lat, networkAreas.currentlySelected.lng),
+            mapTypeId: google.maps.MapTypeId.SATELLITE
+        }
+        var map = new google.maps.Map(document.getElementById(networkAreas.MAP_ELEMENT_ID), myOptions);
+        var marker = new google.maps.Marker({ map: map });
+        var layer = new google.maps.KmlLayer(networkAreas.getKmzUrl(), { preserveViewport: true, suppressInfoWindows: true});
+        layer.setMap(map);
+
+        //Resize Function
+        google.maps.event.addDomListener(window, "resize", function() {
+            var center = map.getCenter();
+            google.maps.event.trigger(map, "resize");
+            map.setCenter(center);
+        });
+
+        google.maps.event.addListener(layer, 'click', function(event) {
+            showInContentWindow(event.latLng, event.featureData.description);
+        });
+
+        function showInContentWindow(position, text) {
+            var content = networkAreas.getPopUpContent(text);
+
+            var infowindow = new google.maps.InfoWindow({
+                content: content,
+                position: position
+            })
+            infowindow.open(map);
+        }
+    }
+
     networkAreas.getPopUpContent = function (text) {
         var descriptions = text.split("~");
-        var longitude = descriptions[0];
-        var latitude = descriptions[1];
-        var fibreHub = descriptions[2];
-        var networkPoint = descriptions[3];
-        var serial = descriptions[4];
-        var tagId = descriptions[5];
-        var redirectUrl = networkAreas.SERVICE_HTML_URL + "tagId=" + tagId  + "&district=" + location + "&long=" + longitude + "&lat=" + latitude +  networkAreas.REGISTRATION_FRAGMENT;
+        var fibreHub = descriptions[0];
+        var networkPoint = descriptions[1];
+        var serial = descriptions[2];
+        var tagId = descriptions[3];
+        var longitude = descriptions[4];
+        var latitude = descriptions[5];
+
+        
+        var redirectUrl = networkAreas.SERVICE_HTML_URL + "tagId=" + tagId  + "&district=" + networkAreas.currentlySelected.title + "&long=" + longitude + "&lat=" + latitude +  networkAreas.REGISTRATION_FRAGMENT;
         var content = ""+
             "<table style='font-family:Arial,Verdana,Times;font-size:12px;text-align:left;width:100%;border-spacing:0px; padding:0px 3px 3px 3px'>"+
             "<tr>"+
@@ -81,39 +123,6 @@ if (typeof(networkAreas) == 'undefined') {
             "</tr>"+
             "</table>";
         return content;
-    }
-
-    networkAreas.initialize = function() {
-        var myOptions = {
-            zoom: 14,
-            center: new google.maps.LatLng(networkAreas.currentlySelected.lat, networkAreas.currentlySelected.lng),
-            mapTypeId: google.maps.MapTypeId.SATELLITE
-        }
-        var map = new google.maps.Map(document.getElementById(networkAreas.MAP_ELEMENT_ID), myOptions);
-        var marker = new google.maps.Marker({ map: map });
-        var layer = new google.maps.KmlLayer(networkAreas.getKmzUrl(), { preserveViewport: true, suppressInfoWindows: true});
-        layer.setMap(map);
-
-        //Resize Function
-        google.maps.event.addDomListener(window, "resize", function() {
-            var center = map.getCenter();
-            google.maps.event.trigger(map, "resize");
-            map.setCenter(center);
-        });
-
-        google.maps.event.addListener(layer, 'click', function(kmlEvent) {
-            showInContentWindow(kmlEvent.latLng, kmlEvent.featureData.description);
-        });
-
-        function showInContentWindow(position, text) {
-            var content = networkAreas.getPopUpContent(text);
-
-            var infowindow = new google.maps.InfoWindow({
-                content: content,
-                position: position
-            })
-            infowindow.open(map);
-        }
     }
 }
 
